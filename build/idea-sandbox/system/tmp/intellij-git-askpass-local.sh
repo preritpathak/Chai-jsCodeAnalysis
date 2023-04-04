afd9426c11e558c0a197c3f1efef2a11445fcbf7
Prerit Pathak <preritpat@hotmail.com>
2023-04-03 22:26:53 -0400

    code cleanup [removed build]

--- build/idea-sandbox/system/tmp/intellij-git-askpass-local.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-"/Applications/WebStorm.app/Contents/jbr/Contents/Home/bin/java" -cp "/Applications/WebStorm.app/Contents/plugins/git4idea/lib/git4idea-rt.jar:/Applications/WebStorm.app/Contents/lib/externalProcess-rt.jar:/Applications/WebStorm.app/Contents/lib/app.jar:/Applications/WebStorm.app/Contents/lib/3rd-party-rt.jar" git4idea.http.GitAskPassApp "$@"
